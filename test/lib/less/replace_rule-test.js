9c45db84dc8faff30829380d8c6f77d4bc52cc62
suisho <suisho000000@gmail.com>
2012-08-11 22:32:52 +0900

    ReplaceRule accept regexp

--- test/lib/less/replace_rule-test.js
+++ test/lib/less/replace_rule-test.js
@@ -2,29 +2,20 @@ var assert = require("assert");
 var ReplaceRule = module.parent.exports.require("./lib/less/replace_rule");
 
 module.exports = {
-  testInit : function(){
-    var rep = new ReplaceRule({"hoge": "fuga"});
-    assert.equal(rep.get("hoge"),"fuga");
-    var rule = {
-      "a" : "b",
-      "c" : "d"
-    };
-    rep.init(rule);
-    assert.equal(rep.get("a"),"b");
-    assert.equal(rep.get("c"),"d");
-    assert.equal(rep.get("hoge"),null);
-  },
-  testAdd : function(){
-    var rep = new ReplaceRule();
-    rep.add("hoge","fuga");
-    assert.equal("fuga",rep.get("hoge"));
-  },
   testReplace : function(){
     var rep = new ReplaceRule();
     rep.add("hoge","fuga");
     rep.add(".less",".cnv.css");
     var result = rep.replace("hoge.less");
     var expect ="fuga.cnv.css";
+    assert.equal(expect, result);
+  },
+  testRegexpReplace : function(){
+    var rep = new ReplaceRule();
+    rep.add(/.less$/,".cnv.css");
+
+    var result = rep.replace("less.less");
+    var expect ="less.cnv.css";
     assert.equal(expect, result);
   }
 }
