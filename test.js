e31b7d52eb364f950e9c37832d919143972e37af
suisho <suisho000000@gmail.com>
2012-08-11 19:37:18 +0900

    Upgrade testing tool

--- test.js
+++ test.js
@@ -1,42 +1,90 @@
 var path = require("path");
 var color = require("colors");
 var util = require("util");
+var fsExtednd = require("./lib/fs_extend");
+// setting
+var failOnError = true;
+var detail = true;
+
+__console_log = console.log;
+console.log = function(message){
+  if(detail){
+    __console_log(message);
+  }
+}
 //module.paths.push(path.resolve("./app/lib"));
 //console.log(module.paths);
-var tests = [
-  "./test/lib/less/include_css-test.js",
-  "./test/lib/less/convert-test.js",
-  "./test/lib/less/replace_rule-test.js",
-];
 
-function testing(testName, testFunc){
+exports.require = function(path){
+  return require(path);
+}
+
+function testing(testName, testFunc, failOnError){
   console.log("Test ".magenta + testName);
   try{
     testFunc();
   }catch(e){
     success = false;
-    console.log(util.inspect(e).red);
-    console.log(e.stack);
+    var inspect = util.inspect(e);
+    console.error(inspect.red);
+    //console.trace();
+    if(e.actual && e.expected){
+      var diff = require("difflet")({"indent":2});
+      diff(e.actual, e.expected).pipe(process.stdout);
+    }
+    if(failOnError){
+      throw e;
+    }
   }
   if(success){
     console.log(" => success".green);
   }
+  return success;
 }
 
-for(var i=0; i < tests.length; i++){
-  var testFile = tests[i];
-  var success = true;
+var testCount = 0;
+var successCount = 0;
 
-  console.log("Start test:".cyan +testFile);
-  var test = require(testFile);
-  for(var key in test){
-    var item = test[key];
-    if(typeof(item) !== "function"){
+var tests = fsExtednd.lsRecursive("./test");
+
+try{
+  for(var i=0; i < tests.length; i++){
+    var testFile = tests[i];
+    if(/-test.js/.test(testFile) == false){
       continue;
     }
-    if(key.match(/test.*/)){
-      testing(key, item);
+    var success = true;
+
+    console.log("Start test:".cyan +testFile);
+    var test = require(testFile);
+    for(var key in test){
+      var item = test[key];
+      if(typeof(item) !== "function"){
+        continue;
+      }
+      if(key.match(/test.*/)){
+        var success = testing(key, item, failOnError);
+        testCount++;
+        if(success){
+          successCount++;
+        }
+      }
     }
+    console.log("End test:".cyan +testFile+"\n");
   }
-  console.log("End test:".cyan +testFile+"\n");
+  detail = true;
+  console.log("Test Result: (successs/all) "+ successCount+"/"+ testCount+"\n");
+  if(successCount == testCount){
+    console.log("Congraturation! All Green!".green);
+  }else{
+    var failCount = testCount - successCount;
+    console.log("Failed !".red);
+    console.log("Failed !!".red);
+    console.log("Failed !!!".red);
+
+    console.log("Faild "+ failCount+" tests");
+  }
+}catch(e){
+  throw e;
+  //console.error(e.toString().red.bold);
 }
