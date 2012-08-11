9ecf21eac267da10d78135e32828f8529004d61a
suisho <suisho000000@gmail.com>
2012-08-12 02:10:18 +0900

    Rename include_css to cssinclude

--- test/lib/less/cssinclude-test.js
+++ test/lib/less/cssinclude-test.js
@@ -2,7 +2,7 @@ var fs = require("fs");
 var path = require("path");
 var assert = require("assert");
 
-var includeCss = module.parent.exports.require("./lib/less/include_css.js").IncludeCss;
+var CssInclude = module.parent.exports.require("./lib/less/cssinclude.js");
 var paths = ["./test/mock/include_test"];
 
 var extendCssString = fs.readFileSync("./test/mock/include_test/extend_test.css","utf-8");
@@ -23,32 +23,32 @@ module.exports = {
     // resolvePath
     testResolvePath : function(){
         var paths = ["not_found_path","./test/mock/include_test"]
-        var resolve = includeCss.resolvePath("hoge.css",paths);
+        var resolve = CssInclude.resolvePath("hoge.css",paths);
         var expect = ["test","mock","include_test","hoge.css"].join(path.sep);
         assert.equal(expect,resolve);
     },
     testResolvePathFalse : function(){
-        var resolve = includeCss.resolvePath("not_resolve_file.css",paths);
+        var resolve = CssInclude.resolvePath("not_resolve_file.css",paths);
         assert.equal(null,resolve);
     },
     testReplaceimport : function(){
         // replaceImport
         convertPatterns.forEach(function(item){
             //console.log("* Test "+item);
-            var cssString = includeCss.replaceImport(item, paths, item);
+            var cssString = CssInclude.replaceImport(item, paths, item);
             assert.equal(hogeCssString, cssString);
         })
     },
     testNotReplaceImport : function(){
         notConvertPatterns.forEach(function(item){
             //console.log("* Test "+item);
-            var cssString = includeCss.replaceImport(item, paths, item);
+            var cssString = CssInclude.replaceImport(item, paths, item);
             assert.equal(item, cssString);
         })
     },
     testExtend : function(){
         // extend
-        var result = includeCss.extend(extendCssString, paths);
+        var result = CssInclude.extend(extendCssString, paths);
         assert.equal(extendCssStringExpect, result);
     }
 }
