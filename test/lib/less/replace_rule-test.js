4ff96e93adb93e76141ddc779c14c5021e668e3e
suisho <suisho000000@gmail.com>
2012-08-11 19:44:53 +0900

    Add Converter

--- test/lib/less/replace_rule-test.js
+++ test/lib/less/replace_rule-test.js
@@ -1,18 +1,30 @@
 var assert = require("assert");
+var ReplaceRule = module.parent.exports.require("./lib/less/replace_rule");
 
-var ReplaceRule = require("../../../lib/less/replace_rule.js");
 module.exports = {
-    testAdd : function(){
-        var rep = new ReplaceRule();
-        rep.add("hoge","fuga");
-        assert.equal("fuga",rep.get("hoge"));
-    },
-    testReplace : function(){
-        var rep = new ReplaceRule();
-        rep.add("hoge","fuga");
-        rep.add(".less",".cnv.css");
-        var result = rep.replace("hoge.less");
-        var expect ="fuga.cnv.css";
-        assert.equal(expect, result);
-    }
+  testInit : function(){
+    var rep = new ReplaceRule({"hoge": "fuga"});
+    assert.equal(rep.get("hoge"),"fuga");
+    var rule = {
+      "a" : "b",
+      "c" : "d"
+    };
+    rep.init(rule);
+    assert.equal(rep.get("a"),"b");
+    assert.equal(rep.get("c"),"d");
+    assert.equal(rep.get("hoge"),null);
+  },
+  testAdd : function(){
+    var rep = new ReplaceRule();
+    rep.add("hoge","fuga");
+    assert.equal("fuga",rep.get("hoge"));
+  },
+  testReplace : function(){
+    var rep = new ReplaceRule();
+    rep.add("hoge","fuga");
+    rep.add(".less",".cnv.css");
+    var result = rep.replace("hoge.less");
+    var expect ="fuga.cnv.css";
+    assert.equal(expect, result);
+  }
 }
